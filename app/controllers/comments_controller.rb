08893bca6ad09a9a83354aa1fcbcb7d5a25f97e0
Jeff Kamo <jeffkamo@mobify.com>
2013-03-25 23:40:38 -0700

    General styling improvements

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,13 +5,7 @@ class CommentsController < ItemsController
     @comments = Comment.all
   end
 
-  # def new
-  #   @comment = Comment.new
-  # end
-
   def create
-    # render :nothing => true
-
     @comment = Comment.new(params[:comment])
     @comment.item = @item
 
